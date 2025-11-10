e503cb99ba358be3e2bd5478ba9551dd236b6adc
Arun <arunonwork3249@gmail.com>
2025-11-10 17:10:15 +0530

    Added Relative Documentation links for Skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {
   SiReact,
   SiNextdotjs,
@@ -12,33 +13,35 @@ import {
 
 
 const skills = [
-  { name: "ReactJs", icon: <SiReact size={30} className="text-blue-500" /> },
+  { name: "ReactJs", icon: <SiReact size={30} className="text-blue-500" />, url: "https://react.dev/" },
   {
     name: "NextJs",
-    icon: <SiNextdotjs size={30} className="text-gray-900 dark:text-white" />,
+    icon: <SiNextdotjs size={30} className="text-gray-900 dark:text-white" />, url: "https://nextjs.org/"
   },
   {
     name: "NodeJs",
-    icon: <SiNodedotjs size={30} className="text-green-500" />,
+    icon: <SiNodedotjs size={30} className="text-green-500" />, url: "https://nodejs.org/en"
   },
   {
     name: "ExpressJs",
-    icon: <SiExpress size={30} className="text-gray-700 dark:text-gray-300" />,
+    icon: <SiExpress size={30} className="text-gray-700 dark:text-gray-300" />, url: "https://expressjs.com/"
   },
-  { name: "MongoDB", icon: <SiMongodb size={30} className="text-green-600" /> },
+  { name: "MongoDB", icon: <SiMongodb size={30} className="text-green-600" />, url: "https://cloud.mongodb.com" },
   {
     name: "React Native",
-    icon: <SiExpo size={30} className="text-blue-400" />,
+    icon: <SiExpo size={30} className="text-blue-400" />, url: "https://reactnative.dev/"
   },
   {
     name: "JavaScript",
     icon: <SiJavascript size={30} className="text-yellow-500" />,
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
   },
   {
     name: "TypeScript",
     icon: <SiTypescript size={30} className="text-blue-600" />,
+    url: "https://www.typescriptlang.org/"
   },
-  { name: "Python", icon: <SiPython size={30} className="text-yellow-400" /> },
+  { name: "Python", icon: <SiPython size={30} className="text-yellow-400" />, url: "https://www.python.org/" },
 ];
 
 const Skills = () => {
@@ -50,13 +53,15 @@ const Skills = () => {
       <h2 className="text-3xl font-bold text-center text-white">My Skills</h2>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-6">
         {skills.map((skill, index) => (
-          <div
+          <Link
+            href={skill.url}
             key={index}
             className="flex flex-col items-center bg-gray-600 text-white p-4 rounded-lg shadow-md hover:bg-gray-400 cursor-pointer "
+            target="_blank"
           >
             {skill.icon}
             <p className="mt-2 text-lg">{skill.name}</p>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
